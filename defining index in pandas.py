b69cd9ece2b8d67bb9a2b86dce2dc84139815193
Ansh2609 <73031691+Ansh2609@users.noreply.github.com>
2021-04-14 13:21:08 -0400

    Add files via upload

---
+++ defining index in pandas.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 14 22:50:19 2021
+
+@author: anshs
+"""
+
+import pandas as pd
+import numpy as np
+D=pd.Series([1,2,3,4,5,6,7,],index=["a","b","c","d","e","f","g"])
+print(D)
